7dced0f2264c7c8dee754047f8d7dc128245f7a1
anthonycota123 <flaco.7828@icloud.com>
2025-06-21 21:54:04 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import DealAnalyzer from "./pages/DealAnalyzer";
-import ContractorFinder from "./pages/ContractorFinder";
-import HardMoney from "./pages/HardMoney";
-import DealMap from "./pages/DealMap";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
-      <Route path="/deal-analyzer" element={<DealAnalyzer />} />
-      <Route path="/contractor-finder" element={<ContractorFinder />} />
-      <Route path="/hard-money" element={<HardMoney />} />
-      <Route path="/deal-map" element={<DealMap />} />
     </Routes>
   );
 }
